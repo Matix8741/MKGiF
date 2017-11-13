3af60d8cd8fa9b35e8bb8dc93a87631bc386c741
Mateusz <matix8741@gmail.com>
2017-11-13 11:40:38 +0100

    working sending json file

---
+++ server/MKGiF_Server/src/com/server/servlet/connection/ConnectionServlet.java
@@ -0,0 +1,63 @@
+package com.server.servlet.connection;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+
+/**
+ * Servlet implementation class ConnectionServlet
+ */
+@WebServlet("/ConnectionServlet")
+public class ConnectionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConnectionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		String message;
+		JSONObject json = new JSONObject();
+		json.put("name", "student");
+		JSONArray array = new JSONArray();
+		JSONObject item = new JSONObject();
+			item.put("information", "test");
+			item.put("id", 3);
+			item.put("name", "course1");
+			json.put("course", array);
+		array.add(item);
+
+
+		message = json.toString();
+
+		out.print(json);
+		out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
