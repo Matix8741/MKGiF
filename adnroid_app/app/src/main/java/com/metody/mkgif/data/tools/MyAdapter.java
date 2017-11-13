35d0511a0095cd2da3b16c22726dd4f1b6e35cb8
Mateusz <matix8741@gmail.com>
2017-11-13 17:03:04 +0100

    new file

---
+++ adnroid_app/app/src/main/java/com/metody/mkgif/data/tools/MyAdapter.java
@@ -0,0 +1,10 @@
+package com.metody.mkgif.data.tools;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Mateusz on 2017-11-13.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter {
+}
---
+++ adnroid_app/app/src/main/java/com/metody/mkgif/data/tools/MyOnClickListener.java
@@ -0,0 +1,10 @@
+package com.metody.mkgif.data.tools;
+
+import android.content.DialogInterface;
+
+/**
+ * Created by Mateusz on 2017-11-13.
+ */
+
+public class MyOnClickListener implements DialogInterface.OnClickListener {
+}
